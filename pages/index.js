7175d74b17ae9df1d7ae6a4822adf2965c102aac
esha7130 <esha7130@uni.sydney.edu.au>
2021-12-31 00:07:45 +1100

    add haowen photo, description and links to resume, portfolio and contact

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import Footer from '/components/Footer'
+import Navbar from '/components/Navbar'
 
 export default function Home() {
   return (
@@ -11,20 +12,46 @@ export default function Home() {
         <title>Hao Wen</title>
         <meta name="description" content="Hao Wen's portfolio" />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel='preload'
+          href='/fonts/NeutralFace/NeutralFace-Bold.otf'
+          as='font'
+          crossOrigin=''
+        />
+        <link
+          rel='preload'
+          href='/fonts/NeutralFace/NeutralFace.otf'
+          as='font'
+          crossOrigin=''
+        />
+        <link
+          rel='preload'
+          href='/fonts/Rokkitt/static/Rokkitt-Regular.ttf'
+          as='font'
+          crossOrigin=''
+        />
       </Head>
-
+      <Navbar/>
       <main className={styles.main}>
-        <div className={styles.showcase}>
-        <Image src="/vercel.svg" alt="Vercel Logo" width={300} height={200} />
-        </div>
         <div className={styles.description}>
+          <Image src="/photo.jpg" alt="photo of Hao Wen" width={494} height={509} />
           <h1 className={styles.title}>
             I'm Hao Wen.
           </h1>
-          An architecture student.
-        </div>
-
-        <div className={styles.grid}>
+          THIRD YEAR ARCHITECTURE STUDENT
+          <div className={styles.links}>
+            <ul>
+              <li>
+                <a href='/resume'>RESUME</a>
+              </li>
+              <li>
+                <a href='/portfolio'>PORTFOLIO</a>
+              </li>
+              <li>
+                <a href='/contact'>CONTACT</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </main>
       <Footer />
