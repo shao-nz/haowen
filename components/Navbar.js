c05442a5153a3bdbb90e3aacc10c4ea1aeed0bad
esha7130 <esha7130@uni.sydney.edu.au>
2021-12-31 00:06:24 +1100

    add initials and instagram clickable image

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,14 +3,23 @@ import Image from "next/image"
 
 export default function NavBar() {
     return (
-        <nav>
+        <nav className={styles.navbar}>
+            <a href='/'>
+                HW.
+            </a>
+            
             <a
                 className={styles.instagram}
                 href='https://www.instagram.com/hao_portfolio/'
                 target="_blank"
                 rel="noopener noreferrer"
             >
-                <Image src='/instagram.png' alt='Instagram logo' width={25} height={25}/>
+            <Image 
+                src='/instagram.png' 
+                alt='Instagram logo' 
+                width={25} 
+                height={25}
+            />
             </a>
         </nav>
     )
