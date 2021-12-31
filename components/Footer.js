9cd2c10cab6636a15480d7783ed7b019c432edc0
esha7130 <esha7130@uni.sydney.edu.au>
2021-12-31 23:49:43 +1100

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,17 +5,7 @@ import Link from "next/link"
 export default function Footer() {
     return (
        <footer className={styles.footer}>
-        <Link
-            href="/"
-            rel="noopener noreferrer">
-            <a>Home Page</a>
-        </Link>
-        <Link
-            href="/about"
-            rel="noopener noreferrer">
-            <a>About Me</a>
-        </Link>
-        
-    </footer>  
+           Made possible by <Link href='https://github.com/YddEd'><a>Eddy Shao</a></Link> and <Link href='https://github.com/SSBdevelopment'><a>Stephen</a></Link>
+        </footer>  
     )
 }
