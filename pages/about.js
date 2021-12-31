8f742516bada4a5434fab9027bd754268c92a6e6
esha7130 <esha7130@uni.sydney.edu.au>
2021-12-31 22:25:51 +1100

    remove footer from about page

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Footer from "/components/footer"
 export default function AboutPage() {
     return (
         <div>About Hao
-            <div><Footer /></div>
+            <div></div>
         </div>
     )
 }
