9f5f93276c2e0dc763152bc33c30d4d0e60e5d26
esha7130 <esha7130@uni.sydney.edu.au>
2022-01-05 02:18:03 +1100

    add header and button to delete individiual pictures

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,5 +1,6 @@
 import Navbar from '/components/Navbar';
 import Footer from '/components/Footer';
+import Head from 'next/head'
 import styles from '../styles/Dashboard.module.css';
 import React from 'react';
 
@@ -27,35 +28,72 @@ export default function Dashboard() {
     // console.log(pictures);
   }
 
-  async function DeletePictures(id) {
+  async function DeletePicture(id) {
     const url='/api/pictures';
+    const data={
+      id: id
+    };
     const params={
       headers:{
         'Content-Type': 'application/json'
       },
+      body: JSON.stringify(data),
       method:'DELETE'
     };
+    const response = await fetch(url, params);
+    const responseData = await response.json();
+    console.log(responseData);
+    GetPictures()
   }
 
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Hao Wen - Dashboard</title>
+        <meta name="description" content="Hao Wen - Dashboard" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel='preload'
+          href='/fonts/NeutralFace/NeutralFace-Bold.otf'
+          as='font'
+          crossOrigin=''
+        />
+        <link
+          rel='preload'
+          href='/fonts/NeutralFace/NeutralFace.otf'
+          as='font'
+          crossOrigin=''
+        />
+        <link
+          rel='preload'
+          href='/fonts/Rokkitt/static/Rokkitt-Regular.ttf'
+          as='font'
+          crossOrigin=''
+        />
+      </Head>
       <Navbar/>
         <div className={styles.main}> 
           <h1>Dashboard - control yo shit in here</h1>
-          <div className={styles.insertImages}>
+          <div className={styles.insertPictures}>
             <h2>Insert yo images</h2>
           </div>
-          <div className={styles.removeImages}>
+          <div className={styles.removePictures}>
             <h2>Remove yo images</h2>
             <button onClick={GetPictures}>Display Images</button>
             <ul>
-            {
-              pictures.map((picture) => {
-                return (
-                    <li id={picture._id} key={picture._id}>{picture._id} {picture.fileName}</li>
-                )
-              })
-            }
+              <div className={styles.allPictures}>
+                {
+                  pictures.map((picture) => {
+                    return (
+                        <div className={picture.fileName} key={picture._id}>
+                          <li id={picture._id}>{picture._id} {picture.fileName}
+                            <button onClick={() => DeletePicture(picture._id)}>Delete</button>
+                          </li>
+                        </div>
+                    )
+                  })
+                }
+              </div>
             </ul>
           </div>
           <div className={styles.updateImages}>
