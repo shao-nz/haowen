d67755042703bfdabac017ccfd621937a783f169
Eddy Shao <esha7130@uni.sydney.edu.au>
2022-09-02 17:08:08 +1000

    add map

--- pages/contact.js
+++ pages/contact.js
@@ -4,8 +4,32 @@ import Link from 'next/link'
 import styles from '../styles/Contact.module.css'
 import Navbar from '/components/Navbar'
 import Footer from '/components/Footer'
+import { useEffect, useRef } from 'react';
+import { Loader } from '@googlemaps/js-api-loader'
 
 export default function Contact() {
+  const googlemap = useRef(null);
+
+  useEffect(() => {
+    const loader = new Loader({
+      apiKey: 'AIzaSyDQrxgW1mgjVB0JT1K8sBXXv48rmI-ZOXY',
+      version: 'weekly',
+    });
+
+    let map;
+    loader.load().then(() => {
+      const google = window.google;
+      map = new google.maps.Map(googlemap.current, {
+        mapId: '14003129647bca5c',
+        center: {lat: -33.8204125, lng: 151.1111421},
+        zoom: 16,
+        fullscreenControl: false,
+        mapTypeControl: false,
+        streetViewControl: false,
+      });
+    });
+  });
+  
   return (
       <div className={styles.container}>
       <Head>
@@ -33,7 +57,8 @@ export default function Contact() {
       </Head>
       <Navbar/>
       <main className={styles.main}>
-        <p>Coming soon...</p>
+        <h3>Find me here:</h3>
+        <div className={styles.map} ref={googlemap}/>
       </main>
       <Footer />
     </div>
